723b8a1edf952dc79b20974676c8dc5a64cb1fcf
Hadirou Tamdamba <73599970+HadirouTamdamba@users.noreply.github.com>
2023-10-10 17:48:38 +0200

    Update Code_final_airbnb_europ_Random-Forest.r

--- Code_final_airbnb_europ_Random-Forest.r
+++ Code_final_airbnb_europ_Random-Forest.r
@@ -17,19 +17,19 @@ model_rf <- randomForest(Price ~ City + Weekdays_WE + room_type + person_capacit
 # Prédictions sur l'ensemble de test
 predictions <- predict(model_rf, newdata = test_data)
 
-# Calculer l'erreur quadratique moyenne (RMSE)
+# Calcul de l'erreur quadratique moyenne (RMSE)
 rmse <- sqrt(mean((predictions - test_data$Price)^2))
 cat("RMSE:", rmse, "\n") 
 
 #En moyenne, les prédictions de notre modèle diffèrent de 189.0294 euros par rapport aux vraies valeurs, ceci peut être satisfaisant compte tenu de la large plage des Prix.
 
 
-# Calculer l'erreur absolue moyenne (MAE)
+# Calcul de l'erreur absolue moyenne (MAE)
 mae <- mean(abs(predictions - test_data$Price))
 cat("MAE:", mae, "\n")
 
 #RMSE (Root Mean Squared Error) et MAE (Mean Absolute Error) sont utilisés pour quantifier l'écart entre 
-#les prédictions d'un modèle et les vraies valeurs.
+#les prédictions d'un modèle et les vraies valeurs. 
 
 saveRDS(model_rf, "model_rf.rds")
 
